76d893bbc5c35643f5bb86f2d136aa2251dd9726
Euan Muir <unterm30@gmail.com>
2021-01-05 15:01:40 +0000

    fix indent

--- entity_group.py
+++ entity_group.py
@@ -37,18 +37,18 @@ class EntityGroup:
     for entity in entity_list:
       for group in self.default_add_ent_group:
         if not entity in self.dict[group]:
-        self.dict[group].add(entity)
+          self.dict[group].add(entity)
 
   def _add_ent_to_own_group(self,entity_list):
     for entity in entity_list:
       if not entity in self.dict[entity.type]:
-       self.dict[entity.type].add(entity)
+        self.dict[entity.type].add(entity)
 
   def _add_ent_from_list(self,entity_list,group_list):
     for entity in entity_list:
       for group in group_list:
         if not entity in self.dict[group]:
-        self.dict[group].add(entity)
+          self.dict[group].add(entity)
 
   def rm_ent_from_all_groups(self, group_list):
     """remove entities in this list of groups from all groups"""
