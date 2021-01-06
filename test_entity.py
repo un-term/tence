397effe0478ca2718f9148d5b6fb9d85f90fbab1
Euan Muir <unterm30@gmail.com>
2021-01-06 20:28:07 +0000

    use state, use entity_group constructor

--- test_entity.py
+++ test_entity.py
@@ -6,14 +6,14 @@ import unittest
 from entity import *
 from constants import *
 from entity_group import EntityGroup
+from game import State
 
 class TestEntity(unittest.TestCase):
 
   def test_line_sprite(self):
 
-    entity_group = EntityGroup()
-
-    entity_group.add_ent([LineSprite(RED,(0,0),(100,100))])
+    entity_group = EntityGroup([LineSprite(RED,(0,0),(100,100))])
+    state = State(entity_group)
 
     result = len(entity_group.dict["all"].sprites())
     self.assertEqual(1, result)
@@ -38,46 +38,48 @@ class TestEntity(unittest.TestCase):
 
 
     self.assertNotEqual(bad1_pos, result)
-  def test_turret_shooting(self):
 
-    entity_group = EntityGroup()
+
+  def test_turret_shooting(self):
+    """CHANGE: assumes baddie is within range of turret"""
 
     ent_init_list = [
       Baddie((150,150),speed=0.01),
       Turret((100,100)),
       Core((50,50))
     ]
-    entity_group.add_ent(ent_init_list)
+    entity_group = EntityGroup(ent_init_list)
+    state = State(entity_group)
 
-    health = entity_group.dict["baddie"].sprites()[0].health #[0] first and only in list
-    shoot_damage = entity_group.dict["turret"].sprites()[0].damage
+    health = state.entity_group.dict["baddie"].sprites()[0].health #[0] first and only in list
+    shoot_damage = state.entity_group.dict["turret"].sprites()[0].damage
 
-    entity_group.get_group("all").update(step_time=1,total_time=1)
-    entity_group.get_group("all").update(step_time=1,total_time=2)
+    state.entity_group.get_group("all").update(step_time=1,total_time=1)
+    state.entity_group.get_group("all").update(step_time=1,total_time=2)
 
-    result = entity_group.dict["baddie"].sprites()[0].health
+    result = state.entity_group.dict["baddie"].sprites()[0].health
 
     health = health-shoot_damage*2
 
     self.assertEqual(health, result)
 
   def test_core_damage(self):
-    entity_group = EntityGroup()
+
     ent_init_list = [
       Baddie((100,100),speed=10),
       Core((50,50))
     ]
+    entity_group = EntityGroup(ent_init_list)
+    state = State(entity_group)
 
-    entity_group.add_ent(ent_init_list)
-
-    core_health = entity_group.dict["core"].sprites()[0].health
-    baddie_damage = entity_group.dict["baddie"].sprites()[0].damage
+    core_health = state.entity_group.dict["core"].sprites()[0].health
+    baddie_damage = state.entity_group.dict["baddie"].sprites()[0].damage
 
     # baddies do collision detection before moving
-    entity_group.get_group("all").update(step_time=3,total_time=3)
-    entity_group.get_group("all").update(step_time=3,total_time=6)
+    state.entity_group.get_group("all").update(step_time=3,total_time=3)
+    state.entity_group.get_group("all").update(step_time=3,total_time=6)
 
-    result = entity_group.dict["core"].sprites()[0].health
+    result = state.entity_group.dict["core"].sprites()[0].health
     core_health -= baddie_damage
 
     self.assertEqual(core_health, result)
