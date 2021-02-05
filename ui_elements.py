37a2dfb57196303820f5ee628396743ff6a851b6
Euan Muir <unterm30@gmail.com>
2021-02-05 14:03:05 +0000

    Kill counter

--- ui_elements.py
+++ ui_elements.py
@@ -3,3 +3,17 @@ import pygame
 from constants import *
 from general_functions import *
 import entity
+
+class KillCount(pygame.sprite.Sprite):
+    def __init__(self,gui):
+        # Call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self)
+        self.gui = gui
+        self.font = pygame.font.Font(pygame.font.get_default_font(),20)
+        self.image = None
+        self.rect = None
+
+    def update(self):
+        self.image = self.font.render(str(self.gui.state.kill_count), True, GREEN, BLUE)
+        self.rect = self.image.get_rect()
+        # self.rect.topleft = self.gui.screen.topleft
