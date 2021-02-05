40cf41749401fefb185f243b1eea700c3790a1e7
Euan Muir <unterm30@gmail.com>
2021-02-05 14:04:12 +0000

    Core health

--- ui_elements.py
+++ ui_elements.py
@@ -17,3 +17,18 @@ class KillCount(pygame.sprite.Sprite):
         self.image = self.font.render(str(self.gui.state.kill_count), True, GREEN, BLUE)
         self.rect = self.image.get_rect()
         # self.rect.topleft = self.gui.screen.topleft
+
+class CoreHealth(pygame.sprite.Sprite):
+    def __init__(self,gui):
+        # Call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self)
+        self.gui = gui
+        self.font = pygame.font.Font(pygame.font.get_default_font(),20)
+        self.image = None
+        self.rect = None
+
+    def update(self):
+        core = self.gui.state.entity_group.get_group("core").sprites()[0] #show 1 core only
+        self.image = self.font.render(str(int(core.health)), True, RED, BLUE)
+        self.rect = self.image.get_rect()
+        self.rect.bottomright = self.gui.rect.bottomright
